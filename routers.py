531e995aced358b1c6cfa9f220dbaa1d8f757eb6
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2024-02-21 10:35:18 +0100

    Validação de path parameter

--- routers.py
+++ routers.py
@@ -1,5 +1,5 @@
 from asyncio import gather
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from converter import sync_converter, async_converter
 
 router = APIRouter(prefix='/converter')
@@ -22,7 +22,11 @@ def converter(from_currency: str, to_currencies: str, price: float):
     return result
 
 @router.get('/async/{from_currency}')
-async def async_converter_router(from_currency: str, to_currencies: str, price: float):
+async def async_converter_router(
+    from_currency: str = Path(max_length=3, regex='^[A-Z]{3}$'), 
+    to_currencies: str, 
+    price: float
+    ):
     to_currencies = to_currencies.split(',')
 
     coroutines = []
