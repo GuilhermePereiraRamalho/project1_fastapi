47bd2f2a0aa767249221cf2f3ff8f339d5704a32
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2024-02-21 10:38:05 +0100

    Validação de query parameter

--- routers.py
+++ routers.py
@@ -1,5 +1,5 @@
 from asyncio import gather
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, Query
 from converter import sync_converter, async_converter
 
 router = APIRouter(prefix='/converter')
@@ -24,8 +24,8 @@ def converter(from_currency: str, to_currencies: str, price: float):
 @router.get('/async/{from_currency}')
 async def async_converter_router(
     from_currency: str = Path(max_length=3, regex='^[A-Z]{3}$'), 
-    to_currencies: str, 
-    price: float
+    to_currencies: str = Query(max_length=50, regex='^[A-Z]{3}(,[A-Z]{3})*$'), 
+    price: float = Query(gt=0)
     ):
     to_currencies = to_currencies.split(',')
 
